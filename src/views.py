4c746f23992d926ed1b70205a1110b75937ab14a
Jake Armendariz <jakearmendariz99@gmail.com>
2021-01-24 15:04:22 -0800

    ;)

--- src/views.py
+++ src/views.py
@@ -14,6 +14,9 @@ DAILY_MIX = '37i9dQZF1E38bNvW8cIxrb'
 USERNAME = 'svirl3jiigsae2f476alnlfgm'
 SCOPE = 'playlist-read-private playlist-modify-public playlist-read-collaborative playlist-modify-private'
 
+DANNY = 'sewcb0rqq10yisy7c4e8v3ajh'
+DANNY_DAILY = '37i9dQZF1E39FDiPemdR4F'
+DANNY_SAVE = '3d07iaqjCWRfxw5tSHGyZm'
 
 global spotify
 
@@ -29,43 +32,33 @@ def home():
     return redirect(SpotifyOAuth(scope=scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, 
         redirect_uri=REDIRECT_URI, username=USERNAME).get_authorize_url())
 
-
 @app.route('/callback/')
 def success():
-    global spotify
     code = request.args.get('code')
     response = spotify.authorize(code)
-    print('callback headers', spotify.access_token_headers())
-    response = requests.get(f'https://api.spotify.com/v1/playlists/{DAILY_MIX}/tracks', 
-        headers = spotify.access_token_headers())
-    print('sending headers', spotify.access_token_headers())
-    if response.status_code != 200:
+    download_daily_mix()
+    if response[1] != 200:
         return json.dumps({'error': response.reason}), response.status_code
     else:
-        response = response.json()
-        uris = []
-        for item in response['items']:
-            print(item['track']['id'],'is', item['track']['name'])
-            uris.append('spotify:track:' + str(item['track']['id']))
-        res = requests.post(f'https://api.spotify.com/v1/playlists/{SAVED_ID}/tracks', 
-            json = {'uris':uris},
-            headers = spotify.access_token_headers())
-        if res.status_code != 200:
-            return json.dumps({'error adding songs': res.reason}), res.status_code
-        else:
-            return json.dumps({'success':'read daily mix', 'response': res}, indent=2), 200
+        return json.dumps({'success':f'login to user{USERNAME}'})
+    
 
 @app.route('/read_daily/')
-def read_daily():
-    global spotify
-    response = requests.get(f'https://api.spotify.com/v1/playlists/{NAMING}/tracks', 
+def download_daily_mix():
+    print('headers', spotify.access_token_headers())
+    response = requests.get(f'https://api.spotify.com/v1/playlists/{DAILY_MIX}/tracks', 
         headers = spotify.access_token_headers())
-    print('sending headers', spotify.access_token_headers())
     if response.status_code != 200:
         return json.dumps({'error': response.reason}), response.status_code
     else:
         response = response.json()
-        return json.dumps({'success':'read daily mix', 'response': response}), 200
-
-
-    
+    uris = []
+    for item in response['items']:
+        uris.append('spotify:track:' + str(item['track']['id']))
+    res = requests.post(f'https://api.spotify.com/v1/playlists/{SAVED_ID}/tracks', 
+        json = {'uris':uris},
+        headers = spotify.access_token_headers())
+    if res.status_code != 201:
+        return json.dumps({'error adding songs': res.reason}), res.status_code
+    else:
+        return json.dumps({'success':'read daily mix', 'response': res}, indent=2), 200
